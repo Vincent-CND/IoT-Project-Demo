c506de435ad07513b7149f961a57ca48bdd0edcd
The Vinh Nguyen <thevinhnguyen0203@gmail.com>
2026-02-27 12:09:06 +0000

    add tasks

--- public/deploy.js
+++ public/deploy.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function () {
 
   const assignedCheck = document.getElementById("assignedCheck");
@@ -27,3 +28,191 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
 });
+
+$(document).ready(() => {
+
+  let pool = null;
+  //network for dhcp 
+  let network = null;
+  let defaultRouter = null;
+  let dnsServer = null;
+  let excluded = null;
+  let vlanNum = null; 
+  let vlanName = null ; 
+  let userName = null ; 
+  let secret = null; 
+  let ospfNum = null;
+  let wildcast = null;
+  //Network for ospf
+  let Network = null;
+  let area = null;
+  let ntpServer = null;
+  let flagAssign = false;
+  let startAssignPort = null;
+  let endAssignPort = null;
+  let trunkFlag = false;
+  let userCreateOption = null;
+  let 
+
+  //DHCP Section
+  $("#pool").on("input" , function() {
+    pool = $(this).val();
+    console.log("pool " ,pool);
+  })
+
+  $("#Network").on("input" , function() {
+    network = $(this).val();
+    console.log("network " ,network);
+  })
+
+  $("#defaultRouter").on("input" , function() {
+    defaultRouter = $(this).val();
+    console.log("default Router " , defaultRouter);
+  })
+
+  $("#dnsServer").on("input" , function() {
+    dnsServer = $(this).val();
+    console.log("dns Server " , dnsServer);
+  })
+
+  $("#excluded").on("input" , function() { 
+    excluded = $(this).val()
+    console.log("excluded" , excluded);
+  })
+
+
+  //********** */
+
+  //VLAN Section
+  $("#vlanNum").on("input" , function() { 
+    vlanNum = $(this).val()
+    console.log("vlan num" , vlanNum);
+  })
+
+  $("#vlanName").on("input" , function() { 
+    vlanName = $(this).val()
+    console.log("vlan name" , vlanName);
+  })
+
+  $("#assignedCheck").click( () => {
+    flagAssign = !flagAssign
+    if (flagAssign === true){
+      console.log("got it")
+
+      $("#startPort").on("change", function() {
+      startAssignPort = $(this).val();
+      console.log("Start Port:", startAssignPort);
+
+      $("#endPort").on("change", function() {
+      endAssignPort = $(this).val();
+      console.log("End Port:", endAssignPort);});
+  });
+    } else{
+      console.log("nah")
+    }
+  })
+
+  $("#trunkCheck").click( () => {
+    trunkFlag = !trunkFlag
+    if(trunkFlag === true){
+      console.log("checked")
+    } else{
+      console.log("unchecked")
+    }
+  })
+
+  // $("#startPort").on("change", function() {
+  //   startAssignPort = $(this).val();
+  //   console.log("Start Port:", startAssignPort);
+  // });
+
+  // $("#endPort").on("change", function() {
+  //   endAssignPort = $(this).val();
+  //   console.log("End Port:", endAssignPort);
+  // });
+
+  //******** */
+
+  //user create
+  $("input[name='device']").on("change", function () {
+    userCreateOption = this.id
+    console.log(userCreateOption);
+  });
+  
+
+  $("#userName").on("input" , function() { 
+    userName = $(this).val()
+    console.log("user name" , userName);
+  })
+
+  $("#secret").on("input" , function() { 
+    secret = $(this).val()
+    console.log("secret" , secret);
+  })
+
+  //*****/
+  //ospf 
+  $("#ospfNum").on("input" , function() { 
+    ospfNum = $(this).val()
+    console.log("ospf num" , ospfNum);
+  })
+
+  $("#wildcard").on("input" , function() { 
+    wildcast = $(this).val()
+    console.log("wildcast" ,  wildcast);
+  })
+
+  $("#network").on("input" , function() { 
+    Network = $(this).val()
+    console.log("network" , Network);
+  })
+
+  $("#area").on("input" , function() { 
+    area = $(this).val()
+    console.log("area" , area);
+  })
+
+  //****/
+  //ntp 
+
+  $("#ntpserver").on("input" , function() { 
+    ntpServer = $(this).val()
+    console.log("ntpserver" , ntpServer);
+  })
+
+  //****/
+
+  //deploy button
+  // $("#deployBtn").on("click", () => {
+  //   console.log("ahihi")
+  // })
+
+  $("#deployBtn").click( async function () {
+  let totalConfigData = {
+    pool : pool,
+    network_dhcp : network,
+    default_router : defaultRouter,
+    ipDHCPexcluded : excluded,
+    vlan_number : vlanNum,
+    vlan_name : vlanName,
+    flag_assign : flagAssign,
+    flag_trunk : trunkFlag,
+    start_Assign_Port: startAssignPort,
+    end_Assign_Port : endAssignPort,
+    user_create_option : userCreateOption,
+    user_name : userName,
+    Secret : secret,
+    ospf_num : ospfNum,
+    wildcard : wildcast,
+    network_ospf : Network,
+    Area : area,
+    ntp_server : ntpServer
+  }  
+  const sendData = await axios.post("http://localhost:3000/deploy",totalConfigData)
+  
+  const repliesData = sendData.data
+  console.log(repliesData)
+});
+
+
+})  
--- server.js
+++ server.js
@@ -1,30 +1,135 @@
+//task 1 : dhcp l3 switch or router => option 1 or 2
+//task 2 : vlan ( l3 , 2 l2) => option can choose whatever 
+//task 3 : ospf : l3 or Router => option 1 or 2
+//task 4 : ntp : l3 or Router => option 1 or 2
+//task 5 : save the data to database => have a 
+
 import express from "express";
-import bodyParser from "body-parser";
 import path from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
+import YAML from "yaml";
+import { execFile } from "child_process";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+const USE_WSL = false;
+const ANSIBLE_CMD = USE_WSL ? "wsl" : "ansible-playbook";
+
+const INVENTORY = path.join(__dirname, "inventory.ini");
+const PLAYBOOK = path.join(__dirname, "site.yml");
+const VARS_FILE = path.join(__dirname, "vars.yml");
+
+// Middleware parse body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
+app.get("/", (req, res) => { res.render("index.ejs"); }) 
+app.get("/main", (req,res) => { res.render("main.ejs") }) 
+app.get("/about", (req,res) => { res.render("about.ejs") })
 
+app.post("/deploy", (req, res) => {
+  console.log("==> /deploy called");
+  console.log("body:", req.body);
 
-app.get("/main", (req,res) => {
-  res.render("main.ejs")
-})
+  const vars = {
+    pool: req.body.pool_data,
+    network_dhcp: req.body.network_dhcp,
+    default_router: req.body.default_router,
+    vlan_number: Number(req.body.vlan_number),
+    vlan_name: req.body.vlan_name,
+    flag_trunk: req.body.flag_trunk === true || req.body.flag_trunk === "true",
+    flag_assign: req.body.flag_assign === true || req.body.flag_assign === "true",
+    start_Assign_Port: Number(req.body.start_Assign_Port),
+    end_Assign_Port: Number(req.body.end_Assign_Port),
+    ospf_num: Number(req.body.ospf_num),
+    network_ospf: req.body.network_ospf,
+    wildcard: req.body.wildcard,
+    Area: Number(req.body.Area),
+    ntp_server: req.body.ntp_server,
+  };
 
-app.get("/about", (req,res) => {
-  res.render("about.ejs")
-})
+  // 2) Write vars.yml
+  fs.writeFileSync(VARS_FILE, YAML.stringify(vars), "utf8");
+
+  // 3) Write site.yml (test local)
+  const siteYml = `---
+- name: Test local machine
+  hosts: local
+  gather_facts: no
+
+  tasks:
+    - name: Run echo command
+      command: echo "Ansible is working"
+      register: output
+
+    - name: Show result
+      debug:
+        var: output.stdout
+`;
+
+// const siteYml = `---
+// - name: Gather show commands
+//   hosts: ios_switches
+//   gather_facts: no
+//   collections:
+//     - cisco.ios
+
+//   tasks:
+//     - name: collect infos
+//       cisco.ios.ios_command:
+//         commands:
+//           - show version
+//           - show ip interface brief
+//           - show ip route summary
+//           - show cdp neighbors
+//           - show spanning-tree summary
+//       register: show_output
+
+//     - name: Print output
+//       debug:
+//         var: show_output.stdout_lines
+// `;
+  fs.writeFileSync(PLAYBOOK, siteYml, "utf8");
+
+  // 4) Build args + log
+  const args = USE_WSL
+    ? ["ansible-playbook", "-i", INVENTORY, PLAYBOOK, "-e", `@${VARS_FILE}`]
+    : ["-i", INVENTORY, PLAYBOOK, "-e", `@${VARS_FILE}`];
+
+  console.log("ANSIBLE_CMD:", ANSIBLE_CMD);
+  console.log("ARGS:", args);
+
+  // 5) Run ansible
+  execFile(ANSIBLE_CMD, args, { cwd: __dirname }, (err, stdout, stderr) => {
+    console.log("---- STDOUT ----\n", stdout);
+    console.log("---- STDERR ----\n", stderr);
+
+    if (err) {
+      console.log("---- ERROR ----\n", err);
+      return res.status(500).json({
+        ok: false,
+        message: "Ansible failed",
+        error: err.message,
+        stderr,
+        stdout,
+      });
+    }
+
+    return res.json({
+      ok: true,
+      message: "Deploy success",
+      stdout,
+      stderr,
+    });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
